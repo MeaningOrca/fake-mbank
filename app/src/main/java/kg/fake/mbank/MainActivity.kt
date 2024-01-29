7918b7753217e3cffaf48ffc213129ba73e0aafb
Roman <roman.bralec@gmail.com>
2024-01-29 21:07:05 +0600

    base camera preview

--- app/src/main/java/kg/fake/mbank/MainActivity.kt
+++ app/src/main/java/kg/fake/mbank/MainActivity.kt
@@ -3,44 +3,25 @@ package kg.fake.mbank
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import kg.fake.mbank.ui.theme.MBANKTheme
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MBANKTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            val controller = remember { LifecycleCameraController(applicationContext) }
+            controller.bindToLifecycle(LocalLifecycleOwner.current)
+
+            AndroidView(factory = {
+                    PreviewView(it).apply { this.controller = controller }
+                }, modifier = Modifier.fillMaxSize()
+            )
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MBANKTheme {
-        Greeting("Android")
     }
 }
