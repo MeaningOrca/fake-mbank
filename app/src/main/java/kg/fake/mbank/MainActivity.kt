46494611e17255b491faf76bc1f19c0a18c82ac8
Roman <roman.bralec@gmail.com>
2024-01-30 11:51:39 +0600

    fake ui updated

--- app/src/main/java/kg/fake/mbank/MainActivity.kt
+++ app/src/main/java/kg/fake/mbank/MainActivity.kt
@@ -1,7 +1,6 @@
 package kg.fake.mbank
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.camera.view.LifecycleCameraController
@@ -9,12 +8,18 @@ import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -34,14 +39,34 @@ class MainActivity : ComponentActivity() {
                     }, modifier = Modifier.fillMaxSize()
                 )
 
+                Icon(
+                    painterResource(R.drawable.cancel),
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(top = 30.dp, end = 20.dp)
+                )
+
                 Button(
-                    onClick = { Toast.makeText(applicationContext, "Наёбан!", Toast.LENGTH_SHORT).show() }, // NotImplemented
+                    onClick = { },
+
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.White,
+                        contentColor = Color.Black
+                    ),
+                    shape = RoundedCornerShape(16.dp),
+
                     modifier = Modifier
                         .align(Alignment.BottomCenter)
-                        .padding(bottom = 50.dp)
+                        .padding(bottom = 45.dp)
+
                 ) { Text(text = getString(R.string.button_text)) }
-                
-                Image(painterResource(R.drawable.fake_ui), contentDescription = null)
+
+                Image(
+                    painterResource(R.drawable.fake_ui), contentDescription = null,
+                    modifier = Modifier.align(Alignment.Center)
+                )
             }
         }
     }
