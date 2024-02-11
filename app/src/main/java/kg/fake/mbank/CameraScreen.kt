9977d641a606ce95c8e920b844274027a1e844cb
Roman <roman.bralec@gmail.com>
2024-02-11 17:11:58 +0600

    added fake buttons

--- app/src/main/java/kg/fake/mbank/CameraScreen.kt
+++ app/src/main/java/kg/fake/mbank/CameraScreen.kt
@@ -7,18 +7,29 @@ import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
@@ -75,5 +86,39 @@ fun CameraScreen() {
             painterResource(R.drawable.fake_ui), contentDescription = null,
             modifier = Modifier.align(Alignment.Center)
         )
+
+        Row(
+            Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 45.dp)
+                .fillMaxWidth(),
+            Arrangement.SpaceAround
+        ) {
+            FakeButton(
+                painterResource(id = R.drawable.qr_code),
+                stringResource(R.string.my_qr)
+            )
+            FakeButton(
+                painterResource(id = R.drawable.gallery),
+                stringResource(R.string.from_gallery)
+            )
+        }
+    }
+}
+
+@Composable
+fun FakeButton(painter: Painter, text: String) {
+    Button(
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.White,
+            contentColor = Color.Black
+        ),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Row(Modifier.width(110.dp), Arrangement.SpaceEvenly) {
+            Text(text = text, fontWeight = FontWeight.Bold)
+            Image(painter = painter, contentDescription = null, Modifier.width(20.dp))
+        }
     }
 }
